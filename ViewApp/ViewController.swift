d95bfa74a719d1da4f020c6ae47e0676338a1c3f
shujinarazaki <shujinarazaki@protonmail.com>
2020-09-06 19:46:46 +0900

    Initial Commit

---
+++ ViewApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewApp
+//
+//  Created by 楢崎修二 on 2020/09/06.
+//  Copyright © 2020 楢崎修二. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
