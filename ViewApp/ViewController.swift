042b7df9b52db864ca805dfe44dddc406aaa3901
shujinarazaki <shujinarazaki@protonmail.com>
2020-09-06 21:55:35 +0900

    initialize

--- ViewApp/ViewController.swift
+++ ViewApp/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Counter: UILabel!
+    var num: Int = 0
+    
+    @IBAction func IncButton(_ sender: Any) {
+        num += 1
+        Counter.text = String(num)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
